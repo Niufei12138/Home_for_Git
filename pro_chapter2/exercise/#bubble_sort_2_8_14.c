1b6de5663d6aef5b577ba270da7876185775087a
Niufei <niufei12138@gmail.com>
2022-08-14 14:34:12 +0800

    8_14

---
+++ pro_chapter2/exercise/#bubble_sort_2_8_14.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct stu
+    {
+        char name[10];
+        int age;
+    };
+int com_int(const void* e1,const void* e2)
+{
+    return (*(int*)e1-*(int*)e2 );
+}
+int com_float(const void* e1,const void* e2)
+{
+    if((*(float*)e1-*(float*)e2 )<0)
+    {return -1;}
+    if((*(float*)e1-*(float*)e2 )==0)
+    {return 0;}
+    if((*(float*)e1-*(float*)e2 )>0)
+    {return 1;}
+}
+int com_struct_age(const void*e1,const void*e2)
+{
+    return (((struct stu*)e1)->age)-(((struct stu*)e2)->age);
+}
+
+int com_struct_name(const void*e1,const void*e2)
+{
+    return strcmp((((struct stu*)e1)->name),(((struct stu*)e2)->name));
+}
+void test()
+{
+    int arr[]={9,8,5,7,6,1,2,4,3};
+    int i;
+    int sz=sizeof(arr)/sizeof(arr[0]);
+    qsort(arr,sz,sizeof(arr[0]),com_int);
+    for(i=0;i<sz;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+void test2()
+{
+    float arr[]={9.8,8.2,5.4,7.6,6.4,1.1,2.3,4.7,3.5,3.2,3.6};
+    int i;
+    int sz=sizeof(arr)/sizeof(arr[0]);
+    qsort(arr,sz,sizeof(arr[0]),com_float);
+    for(i=0;i<sz;i++)
+    {
+        printf("%.1f  ",arr[i]);
+    }
+    printf("\n");
+}
+
+
+void test3()
+{
+
+    struct stu s[3]={{"zhangsan",15},{"lisi",25},{"wangwu",10}};
+    int i;
+    int sz=sizeof(s)/sizeof(s[0]);
+    qsort(s,sz,sizeof(s[0]),com_struct_age);
+    for(i=0;i<sz;i++)
+    {
+        printf("%d  ",s[i].age);
+    }
+    printf("\n");
+}
+
+void test4()
+{
+
+    struct stu s[3]={{"zhangsan",15},{"lisi",25},{"wangwu",10}};
+    int i;
+    int sz=sizeof(s)/sizeof(s[0]);
+    qsort(s,sz,sizeof(s[0]),com_struct_name);
+    for(i=0;i<sz;i++)
+    {
+        printf("%s  ",s[i].name);
+    }
+    printf("\n");
+}
+int main()
+{
+    test();
+    test2();
+    test3();
+    test4();
+    return 0;
+}
